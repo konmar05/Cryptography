489b439b8322df398bd091e3506188a6f5f998b7
konmar05 <mako96@hotmail.de>
2022-07-19 18:16:38 +0200

    Update 19.07.2022 - classic Crypto

---
+++ src/main/java/classicCrypto/vigenere/Vigenere.java
@@ -0,0 +1,237 @@
+package classicCrypto.vigenere;
+
+public class Vigenere {
+
+    String plaintext;
+    String chypertext;
+    String plaintextAfterDecryption;
+
+    public Vigenere(String plaintext) {
+        this.plaintext = plaintext;
+    }
+
+    public void encrpyt(String key) {
+        char[] tmp = this.plaintext.toCharArray();
+        char[] keyArray = key.toCharArray();
+        int keyLength = key.length();
+        int[] keyIntArray = new int[keyLength];
+        keyIntArray = changeC2I(keyArray);
+        int[] tmpInt = new int[tmp.length];
+        tmpInt = changeC2I(tmp);
+
+        for (int i = 0; i < tmp.length; i++) {
+            tmpInt[i] = (tmpInt[i] + keyIntArray[i % keyLength]) % 26;
+        }
+
+        tmp = changeI2C(tmpInt);
+        this.chypertext = String.valueOf(tmp);
+
+    }
+
+    public void decrypt(String key) {
+        char[] tmp = this.chypertext.toCharArray();
+        char[] keyArray = key.toCharArray();
+        int keyLength = key.length();
+        //int[] keyIntArray = new int[keyLength];
+        int[] keyIntArray = changeC2I(keyArray);
+        //int[] tmpInt = new int[tmp.length];
+        int[] tmpInt = changeC2I(tmp);
+
+        for (int i = 0; i < tmp.length; i++) {
+            tmpInt[i] = (tmpInt[i] - keyIntArray[i % keyLength]) % 26;
+        }
+
+        tmp = changeI2C(tmpInt);
+        this.plaintextAfterDecryption = String.valueOf(tmp);
+    }
+
+    public static int[] changeC2I(char[] input) {
+        int[] tmpInt = new int[input.length];
+
+        for (int i = 0; i < input.length; i++) {
+            switch (input[i]) {
+                case 'A':
+                    tmpInt[i] = 0;
+                    break;
+                case 'B':
+                    tmpInt[i] = 1;
+                    break;
+                case 'C':
+                    tmpInt[i] = 2;
+                    break;
+                case 'D':
+                    tmpInt[i] = 3;
+                    break;
+                case 'E':
+                    tmpInt[i] = 4;
+                    break;
+                case 'F':
+                    tmpInt[i] = 5;
+                    break;
+                case 'G':
+                    tmpInt[i] = 6;
+                    break;
+                case 'H':
+                    tmpInt[i] = 7;
+                    break;
+                case 'I':
+                    tmpInt[i] = 8;
+                    break;
+                case 'J':
+                    tmpInt[i] = 9;
+                    break;
+                case 'K':
+                    tmpInt[i] = 10;
+                    break;
+                case 'L':
+                    tmpInt[i] = 11;
+                    break;
+                case 'M':
+                    tmpInt[i] = 12;
+                    break;
+                case 'N':
+                    tmpInt[i] = 13;
+                    break;
+                case 'O':
+                    tmpInt[i] = 14;
+                    break;
+                case 'P':
+                    tmpInt[i] = 15;
+                    break;
+                case 'Q':
+                    tmpInt[i] = 16;
+                    break;
+                case 'R':
+                    tmpInt[i] = 17;
+                    break;
+                case 'S':
+                    tmpInt[i] = 18;
+                    break;
+                case 'T':
+                    tmpInt[i] = 19;
+                    break;
+                case 'U':
+                    tmpInt[i] = 20;
+                    break;
+                case 'V':
+                    tmpInt[i] = 21;
+                    break;
+                case 'W':
+                    tmpInt[i] = 22;
+                    break;
+                case 'X':
+                    tmpInt[i] = 23;
+                    break;
+                case 'Y':
+                    tmpInt[i] = 24;
+                    break;
+                case 'Z':
+                    tmpInt[i] = 25;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        return tmpInt;
+    }
+
+    public static char[] changeI2C(int[] input) {
+        char[] tmpChar = new char[input.length];
+
+        for (int i = 0; i < input.length; i++) {
+            switch (input[i]) {
+                case 0:
+                    tmpChar[i] = 'A';
+                    break;
+                case 1:
+                    tmpChar[i] = 'B';
+                    break;
+                case 2:
+                    tmpChar[i] = 'C';
+                    break;
+                case 3:
+                    tmpChar[i] = 'D';
+                    break;
+                case 4:
+                    tmpChar[i] = 'E';
+                    break;
+                case 5:
+                    tmpChar[i] = 'F';
+                    break;
+                case 6:
+                    tmpChar[i] = 'G';
+                    break;
+                case 7:
+                    tmpChar[i] = 'H';
+                    break;
+                case 8:
+                    tmpChar[i] = 'I';
+                    break;
+                case 9:
+                    tmpChar[i] = 'J';
+                    break;
+                case 10:
+                    tmpChar[i] = 'K';
+                    break;
+                case 11:
+                    tmpChar[i] = 'L';
+                    break;
+                case 12:
+                    tmpChar[i] = 'M';
+                    break;
+                case 13:
+                    tmpChar[i] = 'N';
+                    break;
+                case 14:
+                    tmpChar[i] = 'O';
+                    break;
+                case 15:
+                    tmpChar[i] = 'P';
+                    break;
+                case 16:
+                    tmpChar[i] = 'Q';
+                    break;
+                case 17:
+                    tmpChar[i] = 'R';
+                    break;
+                case 18:
+                    tmpChar[i] = 'S';
+                    break;
+                case 19:
+                    tmpChar[i] = 'T';
+                    break;
+                case 20:
+                    tmpChar[i] = 'U';
+                    break;
+                case 21:
+                    tmpChar[i] = 'V';
+                    break;
+                case 22:
+                    tmpChar[i] = 'W';
+                    break;
+                case 23:
+                    tmpChar[i] = 'X';
+                    break;
+                case 24:
+                    tmpChar[i] = 'Y';
+                    break;
+                case 25:
+                    tmpChar[i] = 'Z';
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        return tmpChar;
+    }
+
+    public String printCyphertext() {
+        return this.chypertext;
+    }
+
+    public String printPlaintext() {
+        return this.plaintextAfterDecryption;
+    }
+}
--- src/main/java/mainApplication/Application.java
+++ src/main/java/mainApplication/Application.java
@@ -1,14 +1,26 @@
 package mainApplication;
 
 import classicCrypto.polybius.Polybius;
+import classicCrypto.vigenere.Vigenere;
 
 public class Application extends Polybius {
 
     public static void main(String[] args) {
+
+        /*
         plainText = "BEISPIEL";
         System.out.println("Plainttext: " + plainText);
         chiperText = encrypt(plainText);
         System.out.println("Chipertext: " + chiperText);
+        */
+
+        Vigenere vig = new Vigenere("WELTMEISTER");
+        vig.encrpyt("HAUS");
+
+        System.out.println(vig.printCyphertext());
+
+        vig.decrypt("HAUS");
+        System.out.println(vig.printPlaintext());
     }
 }
 
